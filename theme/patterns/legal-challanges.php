282f1bbced676e0757bb60c3a49fe2ca4cdf641b
riddhi-regur <riddhi.jani@regur.net>
2026-05-22 14:43:26 +0530

    section-2 completed

---
+++ theme/patterns/legal-challanges.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Title: Legal Challanges
+ * Slug: lawfirmpro/legal-challanges
+ * Categories: featured
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"100px","bottom":"100px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:100px;margin-bottom:100px"><!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"50px","bottom":"50px"}}}} -->
+    <div class="wp-block-columns alignwide" style="margin-top:50px;margin-bottom:50px"><!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+            <div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+                <div class="wp-block-group"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-000000"}}},"typography":{"fontStyle":"normal","fontWeight":"300","fontSize":"48px","lineHeight":"1.1","letterSpacing":"0px"}},"textColor":"custom-000000","fontFamily":"plus-jakarta-sans"} -->
+                    <h3 class="wp-block-heading has-custom-000000-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:300;letter-spacing:0px;line-height:1.1">Trusted in</h3>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-000000"}}},"typography":{"fontSize":"48px","fontStyle":"normal","fontWeight":"700","lineHeight":"1.1","letterSpacing":"0px"}},"textColor":"custom-000000","fontFamily":"plus-jakarta-sans"} -->
+                    <h2 class="wp-block-heading has-custom-000000-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:700;letter-spacing:0px;line-height:1.1">Legal Challenges.</h2>
+                    <!-- /wp:heading -->
+                </div>
+                <!-- /wp:group -->
+
+                <!-- wp:paragraph -->
+                <p>Suspendisse turpis augue, aliquam eget ligula id, suscipit blandit magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-2-f-2-f-2-f"}}},"typography":{"fontStyle":"normal","fontWeight":"400","fontSize":"16px","lineHeight":"1","letterSpacing":"0px"}},"textColor":"custom-2-f-2-f-2-f","fontFamily":"plus-jakarta-sans"} -->
+                <p class="has-custom-2-f-2-f-2-f-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:400;letter-spacing:0px;line-height:1">Tempor ipsum efficitur posuere rutrum. Suspendisse mollis neque sed orci dignissim, in convallis dui molestie.</p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:buttons -->
+                <div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"custom-000000","style":{"typography":{"fontStyle":"normal","fontWeight":"800","fontSize":"16px","lineHeight":"1","letterSpacing":"0px","textTransform":"uppercase"}},"fontFamily":"plus-jakarta-sans"} -->
+                    <div class="wp-block-button"><a class="wp-block-button__link has-custom-000000-background-color has-background has-plus-jakarta-sans-font-family has-custom-font-size wp-element-button" style="font-size:16px;font-style:normal;font-weight:800;letter-spacing:0px;line-height:1;text-transform:uppercase">Read More ➔</a></div>
+                    <!-- /wp:button -->
+                </div>
+                <!-- /wp:buttons -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:columns -->
+            <div class="wp-block-columns"><!-- wp:column -->
+                <div class="wp-block-column"><!-- wp:image {"id":215,"width":"293px","height":"auto","aspectRatio":"0.6590442685429493","sizeSlug":"full","linkDestination":"none","align":"center"} -->
+                    <figure class="wp-block-image aligncenter size-full is-resized"><img src="http://lawfirmpro.local/wp-content/uploads/2026/05/legal-chalanges-justice.png" alt="" class="wp-image-215" style="aspect-ratio:0.6590442685429493;width:293px;height:auto" /></figure>
+                    <!-- /wp:image -->
+                </div>
+                <!-- /wp:column -->
+
+                <!-- wp:column -->
+                <div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+                    <div class="wp-block-group"><!-- wp:image {"id":216,"sizeSlug":"full","linkDestination":"none"} -->
+                        <figure class="wp-block-image size-full"><img src="http://lawfirmpro.local/wp-content/uploads/2026/05/legal-challanges-order.png" alt="" class="wp-image-216" /></figure>
+                        <!-- /wp:image -->
+                    </div>
+                    <!-- /wp:group -->
+
+                    <!-- wp:image {"id":217,"sizeSlug":"full","linkDestination":"none"} -->
+                    <figure class="wp-block-image size-full"><img src="http://lawfirmpro.local/wp-content/uploads/2026/05/legal-chalanges-lawyer.png" alt="" class="wp-image-217" /></figure>
+                    <!-- /wp:image -->
+                </div>
+                <!-- /wp:column -->
+            </div>
+            <!-- /wp:columns -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
