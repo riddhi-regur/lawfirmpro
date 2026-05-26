2747d57d2b4f1837b6414c87c0e644c0aaecb063
riddhi-regur <riddhi.jani@regur.net>
2026-05-26 10:11:05 +0530

    layout 1 completed

--- theme/patterns/practice-areas.php
+++ theme/patterns/practice-areas.php
@@ -9,57 +9,7 @@
  */
 ?>
 <!-- wp:group {"style":{"spacing":{"margin":{"top":"100px"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="margin-top:100px"><!-- wp:group {"align":"wide","style":{"border":{"top":{"width":"1px","color":"var:preset|color|custom-cecece"},"right":{"width":"0px","style":"none"},"bottom":{"width":"0px","style":"none"},"left":{"width":"0px","style":"none"}},"spacing":{"padding":{"top":"30px","bottom":"30px"}}},"layout":{"type":"constrained"}} -->
-    <div class="wp-block-group alignwide" style="border-top-color:var(--wp--preset--color--custom-cecece);border-top-width:1px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-style:none;border-left-width:0px;padding-top:30px;padding-bottom:30px"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-        <div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-            <div class="wp-block-group"><!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"48px","fontStyle":"normal","fontWeight":"500","lineHeight":"1.1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
-                <h4 class="wp-block-heading has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1.1">100+</h4>
-                <!-- /wp:heading -->
-
-                <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"16px","lineHeight":"1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
-                <p class="has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1">Successful Cases</p>
-                <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-
-            <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-            <div class="wp-block-group"><!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"48px","lineHeight":"1.1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
-                <h4 class="wp-block-heading has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1.1">1.4K+</h4>
-                <!-- /wp:heading -->
-
-                <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1","fontSize":"16px","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
-                <p class="has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1">Happy Customers</p>
-                <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-
-            <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-            <div class="wp-block-group"><!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"48px","lineHeight":"1.1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
-                <h4 class="wp-block-heading has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1.1">30+</h4>
-                <!-- /wp:heading -->
-
-                <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1","fontSize":"16px","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
-                <p class="has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1">Years of Legal Experience</p>
-                <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-
-            <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-            <div class="wp-block-group"><!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"48px","lineHeight":"1.1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
-                <h4 class="wp-block-heading has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1.1">1.4K+</h4>
-                <!-- /wp:heading -->
-
-                <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1","fontSize":"16px","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
-                <p class="has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1">Happy Customers</p>
-                <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-        </div>
-        <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
-
-    <!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"backgroundColor":"custom-000000","textColor":"background","layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-top:100px"><!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"backgroundColor":"custom-000000","textColor":"background","layout":{"type":"default"}} -->
     <div class="wp-block-group alignfull has-background-color has-custom-000000-background-color has-text-color has-background has-link-color"><!-- wp:group {"style":{"spacing":{"padding":{"top":"100px","bottom":"100px"}}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"wrap","justifyContent":"center"}} -->
         <div class="wp-block-group" style="padding-top:100px;padding-bottom:100px"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
             <div class="wp-block-group"><!-- wp:heading {"style":{"typography":{"textAlign":"center","fontSize":"48px","fontStyle":"normal","fontWeight":"700","lineHeight":"1.1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
