879bf5d90108164e17415284f7ae5fda3012d252
riddhi-regur <riddhi.jani@regur.net>
2026-05-27 12:19:13 +0530

    the protect-right and calculation section done

---
+++ theme/patterns/protect-rights.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Title: Protect Rights
+ * Slug: lawfirmpro/protect-rights
+ * Categories: featured
+ * Description: Dynamic protect rights grid.
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"100px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:100px"><!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons alignwide"><!-- wp:button {"textColor":"custom-bcbcbc","className":"is-style-outline","style":{"typography":{"textAlign":"center","fontSize":"16px","fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|custom-bcbcbc"}}},"border":{"width":"1px","color":"#afafaf","radius":{"topLeft":"300px","topRight":"300px","bottomLeft":"300px","bottomRight":"300px"}},"spacing":{"padding":{"top":"var:preset|spacing|xs","bottom":"var:preset|spacing|xs","left":"24px","right":"24px"}}},"fontFamily":"plus-jakarta-sans"} -->
+        <div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-custom-bcbcbc-color has-text-color has-link-color has-border-color has-plus-jakarta-sans-font-family has-text-align-center has-custom-font-size wp-element-button" style="border-color:#afafaf;border-width:1px;border-top-left-radius:300px;border-top-right-radius:300px;border-bottom-left-radius:300px;border-bottom-right-radius:300px;padding-top:var(--wp--preset--spacing--xs);padding-right:24px;padding-bottom:var(--wp--preset--spacing--xs);padding-left:24px;font-size:16px;font-style:normal;font-weight:600">About Us</a></div>
+        <!-- /wp:button -->
+    </div>
+    <!-- /wp:buttons -->
+
+    <!-- wp:heading {"align":"wide","style":{"typography":{"textAlign":"center","fontSize":"48px","fontStyle":"normal","fontWeight":"800"},"spacing":{"padding":{"right":"300px","left":"300px"}}},"fontFamily":"plus-jakarta-sans"} -->
+    <h2 class="wp-block-heading has-text-align-center alignwide has-plus-jakarta-sans-font-family" style="padding-right:300px;padding-left:300px;font-size:48px;font-style:normal;font-weight:800">Protecting Your Rights With&nbsp;Dedication</h2>
+    <!-- /wp:heading -->
+
+    <!-- wp:columns {"verticalAlignment":null,"align":"wide"} -->
+    <div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"center"} -->
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"500"}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
+            <p class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:500">Suspendisse turpis augue, aliquam eget ligula id, suscipit blandit magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"500"}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
+            <p class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:500">Tempor ipsum efficitur posuere rutrum. Suspendisse mollis neque sed orci dignissim, in convallis dui molestie.</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:details {"className":"wp-block-details summary","style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"600"}},"fontFamily":"plus-jakarta-sans"} -->
+            <details class="wp-block-details summary has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:600">
+                <summary>Lacinia tincidunt tristique vivamus aliquet pharetra lacu</summary><!-- wp:paragraph {"placeholder":"Type / to add a hidden block"} -->
+                <p></p>
+                <!-- /wp:paragraph -->
+            </details>
+            <!-- /wp:details -->
+
+            <!-- wp:details {"className":"wp-block-details summary","style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"600"}},"fontFamily":"plus-jakarta-sans"} -->
+            <details class="wp-block-details summary has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:600">
+                <summary>Class aptent taciti sociosqu ad litora torquent</summary><!-- wp:paragraph {"placeholder":"Type / to add a hidden block"} -->
+                <p></p>
+                <!-- /wp:paragraph -->
+            </details>
+            <!-- /wp:details -->
+
+            <!-- wp:buttons -->
+            <div class="wp-block-buttons"><!-- wp:button {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"800","textTransform":"uppercase"}},"fontFamily":"plus-jakarta-sans"} -->
+                <div class="wp-block-button"><a class="wp-block-button__link has-plus-jakarta-sans-font-family has-custom-font-size wp-element-button" style="font-size:16px;font-style:normal;font-weight:800;text-transform:uppercase">Read more 🡲</a></div>
+                <!-- /wp:button -->
+            </div>
+            <!-- /wp:buttons -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:cover {"url":"http://lawfirmpro.local/wp-content/uploads/2026/05/justice.png","id":651,"dimRatio":0,"overlayColor":"secondary","isUserOverlayColor":true,"isDark":false,"sizeSlug":"full","layout":{"type":"constrained"}} -->
+            <div class="wp-block-cover is-light"><img class="wp-block-cover__image-background wp-image-651 size-full" alt="" src="http://lawfirmpro.local/wp-content/uploads/2026/05/justice.png" data-object-fit="cover" /><span aria-hidden="true" class="wp-block-cover__background has-secondary-background-color has-background-dim-0 has-background-dim"></span>
+                <div class="wp-block-cover__inner-container"><!-- wp:image {"id":652,"sizeSlug":"full","linkDestination":"none"} -->
+                    <figure class="wp-block-image size-full"><img src="http://lawfirmpro.local/wp-content/uploads/2026/05/justice-man.png" alt="" class="wp-image-652" /></figure>
+                    <!-- /wp:image -->
+                </div>
+            </div>
+            <!-- /wp:cover -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
