28927fdbde931c40e4134b7f85db1a388eed34b2
riddhi-regur <riddhi.jani@regur.net>
2026-05-28 12:17:48 +0530

    contact section completed

---
+++ theme/patterns/contact-hero.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Title: Contact Hero
+ * Slug: lawfirmpro/contact-hero
+ * Categories: featured
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons alignwide"><!-- wp:button {"textColor":"custom-bcbcbc","className":"is-style-outline","style":{"typography":{"textAlign":"center","fontSize":"16px","fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|custom-bcbcbc"}}},"border":{"width":"1px","color":"#afafaf","radius":{"topLeft":"300px","topRight":"300px","bottomLeft":"300px","bottomRight":"300px"}},"spacing":{"padding":{"top":"var:preset|spacing|xs","bottom":"var:preset|spacing|xs","left":"24px","right":"24px"}}},"fontFamily":"plus-jakarta-sans"} -->
+        <div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-custom-bcbcbc-color has-text-color has-link-color has-border-color has-plus-jakarta-sans-font-family has-text-align-center has-custom-font-size wp-element-button" style="border-color:#afafaf;border-width:1px;border-top-left-radius:300px;border-top-right-radius:300px;border-bottom-left-radius:300px;border-bottom-right-radius:300px;padding-top:var(--wp--preset--spacing--xs);padding-right:24px;padding-bottom:var(--wp--preset--spacing--xs);padding-left:24px;font-size:16px;font-style:normal;font-weight:600">Contact</a></div>
+        <!-- /wp:button -->
+    </div>
+    <!-- /wp:buttons -->
+
+    <!-- wp:heading {"align":"wide","style":{"typography":{"textAlign":"center","fontSize":"48px","fontStyle":"normal","fontWeight":"800"},"spacing":{"padding":{"right":"350px","left":"350px"}}},"fontFamily":"plus-jakarta-sans"} -->
+    <h2 class="wp-block-heading has-text-align-center alignwide has-plus-jakarta-sans-font-family" style="padding-right:350px;padding-left:350px;font-size:48px;font-style:normal;font-weight:800">Shaping the world of things to come</h2>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph {"align":"wide","style":{"typography":{"textAlign":"center","fontSize":"16px","fontStyle":"normal","fontWeight":"500"},"spacing":{"padding":{"right":"270px","left":"270px"}},"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
+    <p class="has-text-align-center alignwide has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="padding-right:270px;padding-left:270px;font-size:16px;font-style:normal;font-weight:500">Suspendisse turpis augue, aliquam eget ligula id, suscipit blandit magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:query {"queryId":26,"query":{"perPage":10,"pages":0,"offset":0,"postType":"contact","order":"asc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[],"format":[]},"metadata":{"categories":["posts"],"patternName":"core/query-standard-posts","name":"Standard"},"align":"wide"} -->
+    <div class="wp-block-query alignwide"><!-- wp:post-template {"layout":{"type":"grid","columnCount":3}} -->
+        <!-- wp:group {"style":{"border":{"width":"2px"},"spacing":{"padding":{"right":"20px","left":"20px","top":"20px","bottom":"20px"}}},"borderColor":"custom-e-4-e-4-e-4","layout":{"type":"constrained","justifyContent":"left"}} -->
+        <div class="wp-block-group has-border-color has-custom-e-4-e-4-e-4-border-color" style="border-width:2px;padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:post-featured-image {"width":"28px","height":"28px","scale":"contain","style":{"color":{"duotone":"var:preset|duotone|dark-grayscale"}}} /-->
+
+            <!-- wp:post-title {"style":{"typography":{"fontSize":"24px","fontStyle":"normal","fontWeight":"800"}},"fontFamily":"plus-jakarta-sans"} /-->
+
+            <!-- wp:post-excerpt {"showMoreOnNewLine":false,"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"500"}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} /-->
+        </div>
+        <!-- /wp:group -->
+        <!-- /wp:post-template -->
+    </div>
+    <!-- /wp:query -->
+</div>
+<!-- /wp:group -->
