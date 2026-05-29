b1cc595c866b42abe4616fe4ede74d1065ef9cfc
riddhi-regur <riddhi.jani@regur.net>
2026-05-29 12:27:26 +0530

    patterns created for team-detail and blog-detail

---
+++ theme/patterns/blog-detail.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * Title: Blog Detail
+ * Slug: lawfirmpro/blog-detail
+ * Categories: featured
+ * Description: Dynamic articles grid.
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"align":"wide","style":{"typography":{"textAlign":"center","fontSize":"48px","fontStyle":"normal","fontWeight":"800"},"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"padding":{"right":"300px","left":"300px"}}},"fontFamily":"plus-jakarta-sans"} -->
+    <h2 class="wp-block-heading has-text-align-center alignwide has-plus-jakarta-sans-font-family" style="padding-right:300px;padding-left:300px;font-size:48px;font-style:normal;font-weight:800">Essential Legal Guidance Before Taking Action</h2>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph {"align":"wide","style":{"typography":{"textAlign":"center","fontSize":"16px","fontStyle":"normal","fontWeight":"500"},"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}},"spacing":{"padding":{"right":"250px","left":"250px"}}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
+    <p class="has-text-align-center alignwide has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="padding-right:250px;padding-left:250px;font-size:16px;font-style:normal;font-weight:500">Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque fermentum tortor urna, at suscipit ipsum eleifend id. Vivamus convallis nisl ex, sed varius elit ultrices a. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed molestie tristique ex. Aliquam eget tempor risus. Phasellus a lacinia augue. Duis urna leo, pretium vel risus vitae, lobortis efficitur quam. Fusce et posuere lectus. Morbi ut nulla vitae nulla molestie hendrerit.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:group {"layout":{"type":"default"}} -->
+    <div class="wp-block-group"><!-- wp:heading {"align":"wide","style":{"typography":{"textAlign":"left","fontSize":"48px","fontStyle":"normal","fontWeight":"800"},"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"padding":{"top":"50px"}},"border":{"top":{"color":"var:preset|color|custom-cecece","width":"1px"}}},"fontFamily":"plus-jakarta-sans"} -->
+        <h2 class="wp-block-heading has-text-align-left alignwide has-plus-jakarta-sans-font-family" style="border-top-color:var(--wp--preset--color--custom-cecece);border-top-width:1px;padding-top:50px;font-size:48px;font-style:normal;font-weight:800">How Legal Changes Impact Your Case</h2>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"500"},"spacing":{"padding":{"right":"350px"}}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
+        <p class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="padding-right:350px;font-size:16px;font-style:normal;font-weight:500">Molestie tristique ex. Aliquam eget tempor risus. Phasellus a lacinia augue. Duis urna leo, pretium vel risus vitae, lobortis efficitur quam. Fusce et posuere lectus. Morbi ut nulla vitae nulla molestie hendrerit.</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:columns -->
+        <div class="wp-block-columns"><!-- wp:column -->
+            <div class="wp-block-column"><!-- wp:list -->
+                <ul class="wp-block-list"><!-- wp:list-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400"}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
+                    <li class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:400">Maecenas dignissim nisi convallis placerat</li>
+                    <!-- /wp:list-item -->
+
+                    <!-- wp:list-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400"}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
+                    <li class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:400">Quis sem cursus nullam efficitur nisl</li>
+                    <!-- /wp:list-item -->
+
+                    <!-- wp:list-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400"}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
+                    <li class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:400">Vivamus nisl consectetur quis scelerisque</li>
+                    <!-- /wp:list-item -->
+                </ul>
+                <!-- /wp:list -->
+            </div>
+            <!-- /wp:column -->
+
+            <!-- wp:column -->
+            <div class="wp-block-column"><!-- wp:list -->
+                <ul class="wp-block-list"><!-- wp:list-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400"}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
+                    <li class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:400">Vestibul accumsan pulvinar felis dignissim</li>
+                    <!-- /wp:list-item -->
+
+                    <!-- wp:list-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400"}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
+                    <li class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:400">Tristique ligula lorem suscipit consectetur</li>
+                    <!-- /wp:list-item -->
+
+                    <!-- wp:list-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400"}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
+                    <li class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:400">Fusce ligula elementum fermentu</li>
+                    <!-- /wp:list-item -->
+                </ul>
+                <!-- /wp:list -->
+            </div>
+            <!-- /wp:column -->
+        </div>
+        <!-- /wp:columns -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:columns -->
+    <div class="wp-block-columns"><!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:image {"id":999,"sizeSlug":"full","linkDestination":"none"} -->
+            <figure class="wp-block-image size-full"><img src="http://lawfirmpro.local/wp-content/uploads/2026/05/blog-detail.png" alt="" class="wp-image-999" /></figure>
+            <!-- /wp:image -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"verticalAlignment":"center"} -->
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"style":{"typography":{"fontSize":"40px","fontStyle":"normal","fontWeight":"800"},"spacing":{"padding":{"right":"200px"}}},"fontFamily":"plus-jakarta-sans"} -->
+            <h2 class="wp-block-heading has-plus-jakarta-sans-font-family" style="padding-right:200px;font-size:40px;font-style:normal;font-weight:800">Committed to Justice and Client Success</h2>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400"},"spacing":{"padding":{"right":"250px"}}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
+            <p class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="padding-right:250px;font-size:16px;font-style:normal;font-weight:400">Suspendisse turpis augue, aliquam eget ligula id, suscipit blandit magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+
+    <!-- wp:heading {"style":{"typography":{"textAlign":"center","fontSize":"32px","fontStyle":"normal","fontWeight":"800"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}},"spacing":{"padding":{"right":"250px","left":"250px"},"margin":{"top":"50px","bottom":"50px"}}},"backgroundColor":"primary","textColor":"secondary","fontFamily":"plus-jakarta-sans"} -->
+    <h2 class="wp-block-heading has-text-align-center has-secondary-color has-primary-background-color has-text-color has-background has-link-color has-plus-jakarta-sans-font-family" style="margin-top:50px;margin-bottom:50px;padding-right:250px;padding-left:250px;font-size:32px;font-style:normal;font-weight:800">“Strong legal representation isn’t just about winning cases - it’s about protecting your rights and securing your future.”</h2>
+    <!-- /wp:heading -->
+
+    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+    <div class="wp-block-group"><!-- wp:group {"style":{"layout":{"selfStretch":"fit","flexSize":""}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"wrap"}} -->
+        <div class="wp-block-group"><!-- wp:image {"id":1013,"sizeSlug":"full","linkDestination":"none"} -->
+            <figure class="wp-block-image size-full"><img src="http://lawfirmpro.local/wp-content/uploads/2026/05/appereance.png" alt="" class="wp-image-1013" /></figure>
+            <!-- /wp:image -->
+
+            <!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"800","fontSize":"32px"},"spacing":{"padding":{"right":"250px"}}},"fontFamily":"plus-jakarta-sans"} -->
+            <h2 class="wp-block-heading has-plus-jakarta-sans-font-family" style="padding-right:250px;font-size:32px;font-style:normal;font-weight:800">How to Prepare for Your First Court Appearance</h2>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400"},"spacing":{"padding":{"right":"200px"}}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
+            <p class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="padding-right:200px;font-size:16px;font-style:normal;font-weight:400">Molestie suspendisse turpis augue, aliquam eget ligula id, suscipit blandit magna pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+
+        <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+        <div class="wp-block-group"><!-- wp:image {"id":1001,"sizeSlug":"full","linkDestination":"none","align":"left"} -->
+            <figure class="wp-block-image alignleft size-full"><img src="http://lawfirmpro.local/wp-content/uploads/2026/05/personal-injuryclaim.png" alt="" class="wp-image-1001" /></figure>
+            <!-- /wp:image -->
+
+            <!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"800","fontSize":"32px"},"spacing":{"padding":{"right":"250px"}}},"fontFamily":"plus-jakarta-sans"} -->
+            <h2 class="wp-block-heading has-plus-jakarta-sans-font-family" style="padding-right:250px;font-size:32px;font-style:normal;font-weight:800">What to Expect During a Personal Injury Claim</h2>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400"},"spacing":{"padding":{"right":"200px"}}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
+            <p class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="padding-right:200px;font-size:16px;font-style:normal;font-weight:400">Molestie suspendisse turpis augue, aliquam eget ligula id, suscipit blandit magna pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
