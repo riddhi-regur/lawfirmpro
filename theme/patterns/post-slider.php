f9b0809de35f107a3d11b8f021980f81ab1cc476
riddhi-regur <riddhi.jani@regur.net>
2026-06-02 16:58:51 +0530

    slider without arrow created

--- theme/patterns/post-slider.php
+++ theme/patterns/post-slider.php
@@ -9,18 +9,7 @@
 ?>
 
 <!-- wp:group {"className":"custom-post-slider","layout":{"type":"default"}} -->
-<div class="wp-block-group custom-post-slider"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"space-between"}} -->
-    <div class="wp-block-buttons"><!-- wp:button {"className":"slider-prev"} -->
-        <div class="wp-block-button slider-prev"><a class="wp-block-button__link wp-element-button">Left</a></div>
-        <!-- /wp:button -->
-
-        <!-- wp:button {"className":"slider-next"} -->
-        <div class="wp-block-button slider-next"><a class="wp-block-button__link wp-element-button">right</a></div>
-        <!-- /wp:button -->
-    </div>
-    <!-- /wp:buttons -->
-
-    <!-- wp:query {"queryId":21,"query":{"perPage":6,"postType":"attorney","order":"desc","orderBy":"date","sticky":"","parents":[],"format":[]}} -->
+<div class="wp-block-group custom-post-slider"><!-- wp:query {"queryId":21,"query":{"perPage":6,"postType":"attorney","order":"desc","orderBy":"date","sticky":"","parents":[],"format":[]}} -->
     <div class="wp-block-query"><!-- wp:post-template -->
         <!-- wp:post-featured-image {"isLink":true} /-->
 
