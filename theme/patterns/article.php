664852ef8dd7b27c0d1a2b5b6b8f23760b028eee
riddhi-regur <riddhi.jani@regur.net>
2026-06-03 17:44:03 +0530

    articles with slides responsive completed

--- theme/patterns/article.php
+++ theme/patterns/article.php
@@ -9,20 +9,17 @@
  */
 ?>
 <!-- wp:group {"style":{"spacing":{"margin":{"top":"100px","bottom":"100px"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="margin-top:100px;margin-bottom:100px"><!-- wp:group {"align":"wide","layout":{"type":"flex","orientation":"vertical","flexWrap":"wrap","justifyContent":"center"}} -->
-    <div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
-        <div class="wp-block-group"><!-- wp:heading {"style":{"typography":{"textAlign":"center","fontSize":"48px","fontStyle":"normal","fontWeight":"700","lineHeight":"1.1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
-            <h2 class="wp-block-heading has-text-align-center has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:700;letter-spacing:0px;line-height:1.1">Latest Articles</h2>
-            <!-- /wp:heading -->
-        </div>
-        <!-- /wp:group -->
+<div class="wp-block-group" style="margin-top:100px;margin-bottom:100px"><!-- wp:heading {"className":"article-heading","style":{"typography":{"textAlign":"center","fontSize":"48px","fontStyle":"normal","fontWeight":"700","lineHeight":"1.1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
+    <h2 class="wp-block-heading has-text-align-center article-heading has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:700;letter-spacing:0px;line-height:1.1">Latest Articles</h2>
+    <!-- /wp:heading -->
 
-        <!-- wp:query {"queryId":117,"query":{"perPage":3,"pages":0,"offset":0,"postType":"article","order":"asc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[],"format":[]},"metadata":{"categories":["posts"],"patternName":"core/query-standard-posts","name":"Standard"}} -->
+    <!-- wp:group {"className":"custom-post-slider","layout":{"type":"default"}} -->
+    <div class="wp-block-group custom-post-slider"><!-- wp:query {"queryId":117,"query":{"perPage":3,"pages":0,"offset":0,"postType":"article","order":"asc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[],"format":[]},"metadata":{"categories":["posts"],"patternName":"core/query-standard-posts","name":"Standard"}} -->
         <div class="wp-block-query"><!-- wp:post-template {"layout":{"type":"grid","columnCount":3}} -->
             <!-- wp:group {"layout":{"type":"constrained"}} -->
             <div class="wp-block-group"><!-- wp:post-featured-image {"isLink":true} /-->
 
-                <!-- wp:post-title {"textAlign":"left","isLink":true,"style":{"typography":{"fontSize":"24px","fontStyle":"normal","fontWeight":"600","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} /-->
+                <!-- wp:post-title {"textAlign":"left","isLink":true,"className":"article-title","style":{"typography":{"fontSize":"24px","fontStyle":"normal","fontWeight":"600","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} /-->
             </div>
             <!-- /wp:group -->
             <!-- /wp:post-template -->
--- theme/patterns/contact.php
+++ theme/patterns/contact.php
@@ -8,65 +8,62 @@
  * Inserter: true
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"100px"}}},"layout":{"type":"constrained","justifyContent":"center"}} -->
-<div class="wp-block-group" style="padding-top:100px"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"center"}} -->
-    <div class="wp-block-group alignwide"><!-- wp:columns {"verticalAlignment":"center"} -->
-        <div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"","style":{"spacing":{"padding":{"right":"100px"}}},"layout":{"type":"default"}} -->
-            <div class="wp-block-column is-vertically-aligned-center" style="padding-right:100px"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"48px","lineHeight":"1.1","letterSpacing":"0px"},"spacing":{"padding":{"bottom":"20px"}}},"fontFamily":"plus-jakarta-sans"} -->
-                <h2 class="wp-block-heading has-plus-jakarta-sans-font-family" style="padding-bottom:20px;font-size:48px;font-style:normal;font-weight:700;letter-spacing:0px;line-height:1.1">Book a Free Consultation</h2>
-                <!-- /wp:heading -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"50px"}}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group" style="padding-top:50px"><!-- wp:columns {"verticalAlignment":"center"} -->
+    <div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"","style":{"spacing":{"padding":{"right":"100px"}}},"layout":{"type":"default"}} -->
+        <div class="wp-block-column is-vertically-aligned-center" style="padding-right:100px"><!-- wp:heading {"className":"contat-heading","style":{"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"48px","lineHeight":"1.1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
+            <h2 class="wp-block-heading contat-heading has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:700;letter-spacing:0px;line-height:1.1">Book a Free Consultation</h2>
+            <!-- /wp:heading -->
 
-                <!-- wp:group {"style":{"spacing":{"padding":{"right":"50px"}}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
-                <div class="wp-block-group" style="padding-right:50px"><!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400","lineHeight":"1","letterSpacing":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|custom-3-b-3-b-3-b"}}}},"textColor":"custom-3-b-3-b-3-b","fontFamily":"plus-jakarta-sans"} -->
-                    <p class="has-custom-3-b-3-b-3-b-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:400;letter-spacing:0px;line-height:1">Pellentesque habitant morbi tristique senectus netus malesuada fames ac turpis egestas.</p>
-                    <!-- /wp:paragraph -->
+            <!-- wp:paragraph {"className":"contact-para","style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.2","letterSpacing":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|accent"}}},"spacing":{"padding":{"right":"20px"}}},"textColor":"accent","fontFamily":"plus-jakarta-sans"} -->
+            <p class="contact-para has-accent-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="padding-right:20px;font-size:16px;font-style:normal;font-weight:400;letter-spacing:0px;line-height:1.2">Pellentesque habitant morbi tristique senectus netus malesuada fames ac turpis egestas.</p>
+            <!-- /wp:paragraph -->
 
-                    <!-- wp:group {"style":{"border":{"width":"1px"},"dimensions":{"minHeight":"0px"},"layout":{"selfStretch":"fit","flexSize":""},"spacing":{"padding":{"top":"20px","bottom":"20px","left":"20px","right":"20px"}}},"borderColor":"custom-cecece","layout":{"type":"flex","flexWrap":"nowrap"}} -->
-                    <div class="wp-block-group has-border-color has-custom-cecece-border-color" style="border-width:1px;min-height:0px;padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:image {"id":30,"sizeSlug":"full","linkDestination":"none","style":{"color":{"duotone":"var:preset|duotone|dark-grayscale"},"layout":{"selfStretch":"fixed","flexSize":"24px"}}} -->
-                        <figure class="wp-block-image size-full"><img src="http://lawfirmpro.local/wp-content/uploads/2026/05/call-header.png" alt="" class="wp-image-30" /></figure>
-                        <!-- /wp:image -->
+            <!-- wp:group {"style":{"spacing":{"padding":{"right":"50px","top":"20px"},"blockGap":"var:preset|spacing|0"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+            <div class="wp-block-group" style="padding-top:20px;padding-right:50px"><!-- wp:group {"style":{"border":{"width":"1px"},"dimensions":{"minHeight":"0px"},"layout":{"selfStretch":"fit","flexSize":""},"spacing":{"padding":{"top":"10px","bottom":"10px","left":"20px","right":"20px"},"blockGap":"10px"}},"borderColor":"accent-6","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+                <div class="wp-block-group has-border-color has-accent-6-border-color" style="border-width:1px;min-height:0px;padding-top:10px;padding-right:20px;padding-bottom:10px;padding-left:20px"><!-- wp:image {"id":30,"sizeSlug":"full","linkDestination":"none","style":{"color":{"duotone":"var:preset|duotone|dark-grayscale"},"layout":{"selfStretch":"fixed","flexSize":"24px"}}} -->
+                    <figure class="wp-block-image size-full"><img src="http://lawfirmpro.local/wp-content/uploads/2026/05/call-header.png" alt="" class="wp-image-30" /></figure>
+                    <!-- /wp:image -->
 
-                        <!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"155px"},"spacing":{"blockGap":"var:preset|spacing|0"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"bottom","justifyContent":"left","flexWrap":"wrap"}} -->
-                        <div class="wp-block-group"><!-- wp:paragraph {"className":"is-style-default","style":{"typography":{"fontSize":"12px","fontStyle":"normal","fontWeight":"500","letterSpacing":"0px","lineHeight":"1"},"layout":{"selfStretch":"fixed"}},"fontFamily":"plus-jakarta-sans"} -->
-                            <p class="is-style-default has-plus-jakarta-sans-font-family" style="font-size:12px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1">Call us today</p>
-                            <!-- /wp:paragraph -->
+                    <!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"155px"},"spacing":{"blockGap":"5px"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"bottom","justifyContent":"left","flexWrap":"wrap"}} -->
+                    <div class="wp-block-group"><!-- wp:paragraph {"className":"contact-para","style":{"typography":{"fontSize":"12px","fontStyle":"normal","fontWeight":"500","letterSpacing":"0px","lineHeight":"1"},"layout":{"selfStretch":"fit"}},"fontFamily":"plus-jakarta-sans"} -->
+                        <p class="contact-para has-plus-jakarta-sans-font-family" style="font-size:12px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1">Call us today</p>
+                        <!-- /wp:paragraph -->
 
-                            <!-- wp:paragraph {"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"700","lineHeight":"1","letterSpacing":"0px"},"layout":{"selfStretch":"fit"}},"fontFamily":"plus-jakarta-sans"} -->
-                            <p class="has-plus-jakarta-sans-font-family" style="font-size:18px;font-style:normal;font-weight:700;letter-spacing:0px;line-height:1">(257) 388-6895</p>
-                            <!-- /wp:paragraph -->
-                        </div>
-                        <!-- /wp:group -->
+                        <!-- wp:paragraph {"className":"contact-para","style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"700","lineHeight":"1","letterSpacing":"0px"},"layout":{"selfStretch":"fit"}},"fontFamily":"plus-jakarta-sans"} -->
+                        <p class="contact-para has-plus-jakarta-sans-font-family" style="font-size:18px;font-style:normal;font-weight:700;letter-spacing:0px;line-height:1">(257) 388-6895</p>
+                        <!-- /wp:paragraph -->
                     </div>
                     <!-- /wp:group -->
                 </div>
                 <!-- /wp:group -->
             </div>
-            <!-- /wp:column -->
-
-            <!-- wp:column {"verticalAlignment":"center"} -->
-            <div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"metadata":{"categories":["featured"],"patternName":"lawfirmpro/contact-form","name":"Contact-Form"},"style":{"spacing":{"padding":{"top":"50px","bottom":"50px"}}},"backgroundColor":"custom-f-6-f-6-f-6","layout":{"type":"default"}} -->
-                <div class="wp-block-group has-custom-f-6-f-6-f-6-background-color has-background" style="padding-top:50px;padding-bottom:50px"><!-- wp:group {"align":"wide","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"top","justifyContent":"center"}} -->
-                    <div class="wp-block-group alignwide"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"48px","lineHeight":"1.1","letterSpacing":"0px","textAlign":"center"}},"fontFamily":"plus-jakarta-sans"} -->
-                        <h2 class="wp-block-heading has-text-align-center has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:700;letter-spacing:0px;line-height:1.1">Get In Touch</h2>
-                        <!-- /wp:heading -->
-
-                        <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-                        <div class="wp-block-group"><!-- wp:shortcode -->
-                            [lawfirmpro_cf7]
-                            <!-- /wp:shortcode -->
-
-                            <!-- wp:wpforms/form-selector {"clientId":"9f897df0-c522-444b-93b8-4e234717c3d1","formId":"510","theme":"default-copy-1-copy-2","themeName":"Default (Copy 2)","copyPasteJsonValue":"{\u0022displayTitle\u0022:false,\u0022displayDesc\u0022:false,\u0022theme\u0022:\u0022default-copy-1-copy-2\u0022,\u0022themeName\u0022:\u0022Default (Copy 2)\u0022,\u0022fieldSize\u0022:\u0022medium\u0022,\u0022backgroundImage\u0022:\u0022none\u0022,\u0022backgroundPosition\u0022:\u0022center center\u0022,\u0022backgroundRepeat\u0022:\u0022no-repeat\u0022,\u0022backgroundSizeMode\u0022:\u0022cover\u0022,\u0022backgroundSize\u0022:\u0022cover\u0022,\u0022backgroundWidth\u0022:\u0022100px\u0022,\u0022backgroundHeight\u0022:\u0022100px\u0022,\u0022backgroundUrl\u0022:\u0022url()\u0022,\u0022backgroundColor\u0022:\u0022rgba( 0, 0, 0, 0 )\u0022,\u0022fieldBorderRadius\u0022:\u00223px\u0022,\u0022fieldBorderStyle\u0022:\u0022solid\u0022,\u0022fieldBorderSize\u0022:\u00221px\u0022,\u0022fieldBackgroundColor\u0022:\u0022#ffffff\u0022,\u0022fieldBorderColor\u0022:\u0022rgba( 0, 0, 0, 0.25 )\u0022,\u0022fieldTextColor\u0022:\u0022rgba( 0, 0, 0, 0.7 )\u0022,\u0022fieldMenuColor\u0022:\u0022#ffffff\u0022,\u0022labelSize\u0022:\u0022medium\u0022,\u0022labelColor\u0022:\u0022rgba( 0, 0, 0, 0.85 )\u0022,\u0022labelSublabelColor\u0022:\u0022rgba( 0, 0, 0, 0.55 )\u0022,\u0022labelErrorColor\u0022:\u0022#d63637\u0022,\u0022buttonSize\u0022:\u0022medium\u0022,\u0022buttonBorderStyle\u0022:\u0022none\u0022,\u0022buttonBorderSize\u0022:\u00221px\u0022,\u0022buttonBorderRadius\u0022:\u00223px\u0022,\u0022buttonBackgroundColor\u0022:\u0022#066aab\u0022,\u0022buttonTextColor\u0022:\u0022#ffffff\u0022,\u0022buttonBorderColor\u0022:\u0022#066aab\u0022,\u0022pageBreakColor\u0022:\u0022#066aab\u0022,\u0022containerPadding\u0022:\u00220px\u0022,\u0022containerBorderStyle\u0022:\u0022none\u0022,\u0022containerBorderWidth\u0022:\u00221px\u0022,\u0022containerBorderColor\u0022:\u0022#000000\u0022,\u0022containerBorderRadius\u0022:\u00223px\u0022,\u0022containerShadowSize\u0022:\u0022none\u0022,\u0022customCss\u0022:\u0022\u0022}"} /-->
-                        </div>
-                        <!-- /wp:group -->
-                    </div>
-                    <!-- /wp:group -->
-                </div>
-                <!-- /wp:group -->
-            </div>
-            <!-- /wp:column -->
+            <!-- /wp:group -->
         </div>
-        <!-- /wp:columns -->
+        <!-- /wp:column -->
+
+        <!-- wp:column {"verticalAlignment":"center","backgroundColor":"accent-5"} -->
+        <div class="wp-block-column is-vertically-aligned-center has-accent-5-background-color has-background"><!-- wp:group {"metadata":{"categories":["featured"],"patternName":"lawfirmpro/contact-form","name":"Contact-Form"},"style":{"spacing":{"padding":{"top":"50px","bottom":"50px"}}},"backgroundColor":"custom-f-6-f-6-f-6","layout":{"type":"default"}} -->
+            <div class="wp-block-group has-custom-f-6-f-6-f-6-background-color has-background" style="padding-top:50px;padding-bottom:50px"><!-- wp:group {"align":"wide","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"top","justifyContent":"center"}} -->
+                <div class="wp-block-group alignwide"><!-- wp:heading {"className":"contat-heading","style":{"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"48px","lineHeight":"1.1","letterSpacing":"0px","textAlign":"center"}},"fontFamily":"plus-jakarta-sans"} -->
+                    <h2 class="wp-block-heading has-text-align-center contat-heading has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:700;letter-spacing:0px;line-height:1.1">Get In Touch</h2>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+                    <div class="wp-block-group"><!-- wp:shortcode -->
+                        [lawfirmpro_cf7]
+                        <!-- /wp:shortcode -->
+
+                        <!-- wp:wpforms/form-selector {"clientId":"9f897df0-c522-444b-93b8-4e234717c3d1","formId":"510","theme":"default-copy-1-copy-2","themeName":"Default (Copy 2)","copyPasteJsonValue":"{\u0022displayTitle\u0022:false,\u0022displayDesc\u0022:false,\u0022theme\u0022:\u0022default-copy-1-copy-2\u0022,\u0022themeName\u0022:\u0022Default (Copy 2)\u0022,\u0022fieldSize\u0022:\u0022medium\u0022,\u0022backgroundImage\u0022:\u0022none\u0022,\u0022backgroundPosition\u0022:\u0022center center\u0022,\u0022backgroundRepeat\u0022:\u0022no-repeat\u0022,\u0022backgroundSizeMode\u0022:\u0022cover\u0022,\u0022backgroundSize\u0022:\u0022cover\u0022,\u0022backgroundWidth\u0022:\u0022100px\u0022,\u0022backgroundHeight\u0022:\u0022100px\u0022,\u0022backgroundUrl\u0022:\u0022url()\u0022,\u0022backgroundColor\u0022:\u0022rgba( 0, 0, 0, 0 )\u0022,\u0022fieldBorderRadius\u0022:\u00223px\u0022,\u0022fieldBorderStyle\u0022:\u0022solid\u0022,\u0022fieldBorderSize\u0022:\u00221px\u0022,\u0022fieldBackgroundColor\u0022:\u0022#ffffff\u0022,\u0022fieldBorderColor\u0022:\u0022rgba( 0, 0, 0, 0.25 )\u0022,\u0022fieldTextColor\u0022:\u0022rgba( 0, 0, 0, 0.7 )\u0022,\u0022fieldMenuColor\u0022:\u0022#ffffff\u0022,\u0022labelSize\u0022:\u0022medium\u0022,\u0022labelColor\u0022:\u0022rgba( 0, 0, 0, 0.85 )\u0022,\u0022labelSublabelColor\u0022:\u0022rgba( 0, 0, 0, 0.55 )\u0022,\u0022labelErrorColor\u0022:\u0022#d63637\u0022,\u0022buttonSize\u0022:\u0022medium\u0022,\u0022buttonBorderStyle\u0022:\u0022none\u0022,\u0022buttonBorderSize\u0022:\u00221px\u0022,\u0022buttonBorderRadius\u0022:\u00223px\u0022,\u0022buttonBackgroundColor\u0022:\u0022#066aab\u0022,\u0022buttonTextColor\u0022:\u0022#ffffff\u0022,\u0022buttonBorderColor\u0022:\u0022#066aab\u0022,\u0022pageBreakColor\u0022:\u0022#066aab\u0022,\u0022containerPadding\u0022:\u00220px\u0022,\u0022containerBorderStyle\u0022:\u0022none\u0022,\u0022containerBorderWidth\u0022:\u00221px\u0022,\u0022containerBorderColor\u0022:\u0022#000000\u0022,\u0022containerBorderRadius\u0022:\u00223px\u0022,\u0022containerShadowSize\u0022:\u0022none\u0022,\u0022customCss\u0022:\u0022\u0022}"} /-->
+                    </div>
+                    <!-- /wp:group -->
+                </div>
+                <!-- /wp:group -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
     </div>
-    <!-- /wp:group -->
+    <!-- /wp:columns -->
 </div>
 <!-- /wp:group -->
