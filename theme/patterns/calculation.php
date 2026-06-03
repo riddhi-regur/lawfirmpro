0d23ed1c9447050bad5928ebd32f8a3b092f81c8
riddhi-regur <riddhi.jani@regur.net>
2026-06-03 15:45:19 +0530

    clculations responsive completed

--- theme/patterns/calculation.php
+++ theme/patterns/calculation.php
@@ -8,52 +8,49 @@
  * Inserter: true
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"margin":{"top":"100px"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="margin-top:100px"><!-- wp:group {"align":"wide","style":{"border":{"top":{"width":"1px","color":"var:preset|color|custom-cecece"},"right":{"width":"0px","style":"none"},"bottom":{"width":"0px","style":"none"},"left":{"width":"0px","style":"none"}},"spacing":{"padding":{"top":"30px","bottom":"30px"}}},"layout":{"type":"constrained"}} -->
-    <div class="wp-block-group alignwide" style="border-top-color:var(--wp--preset--color--custom-cecece);border-top-width:1px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-style:none;border-left-width:0px;padding-top:30px;padding-bottom:30px"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-        <div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
-            <div class="wp-block-group"><!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"48px","fontStyle":"normal","fontWeight":"500","lineHeight":"1.1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
-                <h4 class="wp-block-heading has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1.1">100+</h4>
-                <!-- /wp:heading -->
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"50px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:50px"><!-- wp:group {"style":{"border":{"top":{"color":"var:preset|color|accent-2","width":"1px"},"right":[],"bottom":[],"left":[]},"spacing":{"padding":{"top":"30px","bottom":"30px"}}},"layout":{"type":"grid","columnCount":4}} -->
+    <div class="wp-block-group" style="border-top-color:var(--wp--preset--color--accent-2);border-top-width:1px;padding-top:30px;padding-bottom:30px"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
+        <div class="wp-block-group"><!-- wp:heading {"level":4,"className":"calculations-heading","style":{"typography":{"fontSize":"48px","fontStyle":"normal","fontWeight":"500","lineHeight":"1.1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
+            <h4 class="wp-block-heading calculations-heading has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1.1">100+</h4>
+            <!-- /wp:heading -->
 
-                <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"16px","lineHeight":"1","letterSpacing":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
-                <p class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1">Successful Cases</p>
-                <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
+            <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"16px","lineHeight":"1","letterSpacing":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|accent"}}}},"textColor":"accent","fontFamily":"plus-jakarta-sans"} -->
+            <p class="has-accent-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1">Successful Cases</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
 
-            <!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
-            <div class="wp-block-group"><!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"48px","lineHeight":"1.1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
-                <h4 class="wp-block-heading has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1.1">1.4K+</h4>
-                <!-- /wp:heading -->
+        <!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
+        <div class="wp-block-group"><!-- wp:heading {"level":4,"className":"calculations-heading","style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"48px","lineHeight":"1.1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
+            <h4 class="wp-block-heading calculations-heading has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1.1">30+</h4>
+            <!-- /wp:heading -->
 
-                <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1","fontSize":"16px","letterSpacing":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
-                <p class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1">Happy Customers</p>
-                <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
+            <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1","fontSize":"16px","letterSpacing":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|accent"}}}},"textColor":"accent","fontFamily":"plus-jakarta-sans"} -->
+            <p class="has-accent-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1">Years of Legal Experience</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
 
-            <!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
-            <div class="wp-block-group"><!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"48px","lineHeight":"1.1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
-                <h4 class="wp-block-heading has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1.1">30+</h4>
-                <!-- /wp:heading -->
+        <!-- wp:group {"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
+        <div class="wp-block-group"><!-- wp:heading {"level":4,"className":"calculations-heading","style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"48px","lineHeight":"1.1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
+            <h4 class="wp-block-heading calculations-heading has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1.1">1.4K+</h4>
+            <!-- /wp:heading -->
 
-                <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1","fontSize":"16px","letterSpacing":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
-                <p class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1">Years of Legal Experience</p>
-                <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
+            <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1","fontSize":"16px","letterSpacing":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|accent"}}}},"textColor":"accent","fontFamily":"plus-jakarta-sans"} -->
+            <p class="has-accent-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1">Happy Customers</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
 
-            <!-- wp:group {"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
-            <div class="wp-block-group"><!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"48px","lineHeight":"1.1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
-                <h4 class="wp-block-heading has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1.1">1.4K+</h4>
-                <!-- /wp:heading -->
+        <!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
+        <div class="wp-block-group"><!-- wp:heading {"level":4,"className":"calculations-heading","style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"48px","lineHeight":"1.1","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
+            <h4 class="wp-block-heading calculations-heading has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1.1">1.4K+</h4>
+            <!-- /wp:heading -->
 
-                <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1","fontSize":"16px","letterSpacing":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
-                <p class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1">Happy Customers</p>
-                <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
+            <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1","fontSize":"16px","letterSpacing":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|accent"}}}},"textColor":"accent","fontFamily":"plus-jakarta-sans"} -->
+            <p class="has-accent-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:500;letter-spacing:0px;line-height:1">Happy Customers</p>
+            <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
     </div>
