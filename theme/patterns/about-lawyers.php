50af8bea298eb9a4b164f70723418f16c3d7303e
riddhi-regur <riddhi.jani@regur.net>
2026-06-05 16:46:04 +0530

    about lawyers responsive completed

--- theme/patterns/about-lawyers.php
+++ theme/patterns/about-lawyers.php
@@ -10,15 +10,18 @@
  */
 ?>
 <!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:columns {"verticalAlignment":null,"align":"wide"} -->
+<div class="wp-block-group"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|0"}}}} -->
     <div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"center"} -->
-        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"style":{"typography":{"fontSize":"48px","fontStyle":"normal","fontWeight":"800"}},"fontFamily":"plus-jakarta-sans"} -->
-            <h2 class="wp-block-heading has-plus-jakarta-sans-font-family" style="font-size:48px;font-style:normal;font-weight:800">Lawyers by Your Side, Every Step</h2>
-            <!-- /wp:heading -->
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+            <div class="wp-block-group"><!-- wp:heading {"className":"practice-area-heading ","style":{"typography":{"fontSize":"48px","fontStyle":"normal","fontWeight":"800","lineHeight":"1.2"},"spacing":{"margin":{"right":"var:preset|spacing|lg"}}},"fontFamily":"plus-jakarta-sans"} -->
+                <h2 class="wp-block-heading practice-area-heading has-plus-jakarta-sans-font-family" style="margin-right:var(--wp--preset--spacing--lg);font-size:48px;font-style:normal;font-weight:800;line-height:1.2">Lawyers by Your Side, Every Step</h2>
+                <!-- /wp:heading -->
 
-            <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-464646"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400"}},"textColor":"custom-464646","fontFamily":"plus-jakarta-sans"} -->
-            <p class="has-custom-464646-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="font-size:16px;font-style:normal;font-weight:400">Tempor ipsum efficitur posuere rutrum uspendisse mollis neque sed orci dignissim, in convallis dui molestie. Suspendisse turpis augue, aliquam eget ligula id, suscipit blandit magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-            <!-- /wp:paragraph -->
+                <!-- wp:paragraph {"className":"practice-area-paragraph","style":{"elements":{"link":{"color":{"text":"var:preset|color|accent"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"right":"var:preset|spacing|lg"}}},"textColor":"accent","fontFamily":"plus-jakarta-sans"} -->
+                <p class="practice-area-paragraph has-accent-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="margin-right:var(--wp--preset--spacing--lg);font-size:16px;font-style:normal;font-weight:400">Tempor ipsum efficitur posuere rutrum uspendisse mollis neque sed orci dignissim, in convallis dui molestie. Suspendisse turpis augue, aliquam eget ligula id, suscipit blandit magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
         </div>
         <!-- /wp:column -->
 
