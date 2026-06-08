74ec41dbe517452217e1aaee937c5555d4b1409e
riddhi-regur <riddhi.jani@regur.net>
2026-06-08 12:14:46 +0530

    contact page hero updated

--- theme/patterns/contact-hero.php
+++ theme/patterns/contact-hero.php
@@ -24,19 +24,62 @@
         <p class="has-text-align-center alignwide practice-area-paragraph has-accent-color has-text-color has-link-color has-plus-jakarta-sans-font-family" style="margin-right:250px;margin-left:250px;font-size:16px;font-style:normal;font-weight:500">Suspendisse turpis augue, aliquam eget ligula id, suscipit blandit magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
         <!-- /wp:paragraph -->
 
-        <!-- wp:query {"queryId":26,"query":{"perPage":10,"pages":0,"offset":0,"postType":"contact","order":"asc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[],"format":[]},"metadata":{"categories":["posts"],"patternName":"core/query-standard-posts","name":"Standard"},"align":"wide"} -->
-        <div class="wp-block-query alignwide"><!-- wp:post-template {"layout":{"type":"grid","columnCount":3}} -->
-            <!-- wp:group {"style":{"border":{"width":"2px"},"spacing":{"padding":{"right":"20px","left":"20px","top":"20px","bottom":"20px"}}},"borderColor":"accent-8","layout":{"type":"constrained","justifyContent":"left"}} -->
-            <div class="wp-block-group has-border-color has-accent-8-border-color" style="border-width:2px;padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:post-featured-image {"width":"28px","height":"28px","scale":"contain","style":{"color":{"duotone":"var:preset|duotone|dark-grayscale"}}} /-->
+        <!-- wp:columns -->
+        <div class="wp-block-columns"><!-- wp:column -->
+            <div class="wp-block-column"><!-- wp:group {"style":{"border":{"width":"2px"},"spacing":{"padding":{"right":"150px","left":"20px","top":"20px","bottom":"20px"}}},"borderColor":"accent-8","layout":{"type":"constrained"}} -->
+                <div class="wp-block-group has-border-color has-accent-8-border-color" style="border-width:2px;padding-top:20px;padding-right:150px;padding-bottom:20px;padding-left:20px"><!-- wp:image {"id":2010,"sizeSlug":"full","linkDestination":"none"} -->
+                    <figure class="wp-block-image size-full"><img src="http://lawfirmpro.local/wp-content/uploads/2026/06/Group-9.png" alt="" class="wp-image-2010" /></figure>
+                    <!-- /wp:image -->
 
-                <!-- wp:post-title {"style":{"typography":{"fontSize":"24px","fontStyle":"normal","fontWeight":"800"}},"fontFamily":"plus-jakarta-sans"} /-->
+                    <!-- wp:paragraph {"style":{"typography":{"fontSize":"24px","fontStyle":"normal","fontWeight":"800","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
+                    <p class="has-plus-jakarta-sans-font-family" style="font-size:24px;font-style:normal;font-weight:800;letter-spacing:0px">Location</p>
+                    <!-- /wp:paragraph -->
 
-                <!-- wp:post-excerpt {"showMoreOnNewLine":false,"style":{"elements":{"link":{"color":{"text":"var:preset|color|accent"}}},"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"500"}},"textColor":"accent","fontFamily":"plus-jakarta-sans"} /-->
+                    <!-- wp:shortcode -->
+                    [lawfirmpro_address]
+                    <!-- /wp:shortcode -->
+                </div>
+                <!-- /wp:group -->
             </div>
-            <!-- /wp:group -->
-            <!-- /wp:post-template -->
+            <!-- /wp:column -->
+
+            <!-- wp:column -->
+            <div class="wp-block-column"><!-- wp:group {"style":{"border":{"width":"2px"},"spacing":{"padding":{"right":"150px","left":"20px","top":"20px","bottom":"20px"}}},"borderColor":"accent-8","layout":{"type":"constrained","justifyContent":"left"}} -->
+                <div class="wp-block-group has-border-color has-accent-8-border-color" style="border-width:2px;padding-top:20px;padding-right:150px;padding-bottom:20px;padding-left:20px"><!-- wp:image {"id":2015,"sizeSlug":"full","linkDestination":"none"} -->
+                    <figure class="wp-block-image size-full"><img src="http://lawfirmpro.local/wp-content/uploads/2026/05/Vector.png" alt="" class="wp-image-2015" /></figure>
+                    <!-- /wp:image -->
+
+                    <!-- wp:paragraph {"style":{"typography":{"fontSize":"24px","fontStyle":"normal","fontWeight":"800","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
+                    <p class="has-plus-jakarta-sans-font-family" style="font-size:24px;font-style:normal;font-weight:800;letter-spacing:0px">Phone Number</p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:shortcode -->
+                    [lawfirmpro_phone]
+                    <!-- /wp:shortcode -->
+                </div>
+                <!-- /wp:group -->
+            </div>
+            <!-- /wp:column -->
+
+            <!-- wp:column -->
+            <div class="wp-block-column"><!-- wp:group {"style":{"border":{"width":"2px"},"spacing":{"padding":{"right":"150px","left":"20px","top":"20px","bottom":"20px"}}},"borderColor":"accent-8","layout":{"type":"constrained","justifyContent":"left"}} -->
+                <div class="wp-block-group has-border-color has-accent-8-border-color" style="border-width:2px;padding-top:20px;padding-right:150px;padding-bottom:20px;padding-left:20px"><!-- wp:image {"id":2016,"sizeSlug":"full","linkDestination":"none"} -->
+                    <figure class="wp-block-image size-full"><img src="http://lawfirmpro.local/wp-content/uploads/2026/05/Group-2084.png" alt="" class="wp-image-2016" /></figure>
+                    <!-- /wp:image -->
+
+                    <!-- wp:paragraph {"style":{"typography":{"fontSize":"24px","fontStyle":"normal","fontWeight":"800","letterSpacing":"0px"}},"fontFamily":"plus-jakarta-sans"} -->
+                    <p class="has-plus-jakarta-sans-font-family" style="font-size:24px;font-style:normal;font-weight:800;letter-spacing:0px">Support Email</p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:shortcode -->
+                    [lawfirmpro_email]
+                    <!-- /wp:shortcode -->
+                </div>
+                <!-- /wp:group -->
+            </div>
+            <!-- /wp:column -->
         </div>
-        <!-- /wp:query -->
+        <!-- /wp:columns -->
     </div>
     <!-- /wp:group -->
 </div>
